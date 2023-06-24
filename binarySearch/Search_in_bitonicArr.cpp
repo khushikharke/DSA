3ae656260a54a36252fc6e2d30e5c286695fcdcc
Khushi Kharke <khushi.kharke111@gmail.com>
2023-06-24 16:50:53 +0530

    ughh this was somethinggit add .  search in  a bitonic array

---
+++ binarySearch/Search_in_bitonicArr.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+// Function for binary search in ascending part
+int ascendingBinarySearch(int arr[], int low,int high, int key)
+{
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key)
+            return mid;
+        if (arr[mid] > key)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
+ 
+// Function for binary search in
+// descending part of array
+int descendingBinarySearch(int arr[], int low, int high, int key)
+{
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key)
+            return mid;
+        if (arr[mid] < key)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
+ 
+// finding bitonic point
+int findBitonicPoint(int arr[], int n,int l, int r)
+{
+    int mid;
+    int bitonicPoint = 0;
+    mid = (r + l) / 2;
+    if (arr[mid] > arr[mid - 1]
+        && arr[mid] > arr[mid + 1])
+    {
+        return mid;
+    }
+ 
+    else if (arr[mid] > arr[mid - 1]
+            && arr[mid] < arr[mid + 1])
+            //right half
+    {
+        bitonicPoint = findBitonicPoint(arr, n, mid, r);
+    }
+ 
+    else if (arr[mid] < arr[mid - 1]
+            && arr[mid] > arr[mid + 1])
+            // left half
+    {
+        bitonicPoint = findBitonicPoint(arr, n, l, mid);
+    }
+    return bitonicPoint;
+}
+ 
+// Function to search key in
+// bitonic array
+int searchBitonic(int arr[], int n,int key, int index)
+{
+    if (key > arr[index])
+    // an index greater than max index is not possible 
+        return -1;
+ 
+    else if (key == arr[index])
+        return index;
+ 
+    else {
+        int firsthalf= ascendingBinarySearch(arr,0, index - 1,key);
+        if (firsthalf != -1) {
+            return firsthalf;
+        }
+ 
+        // Search in right of k
+        return descendingBinarySearch(arr,  index + 1,n - 1,key);
+    }
+}
+
+int main()
+{
+    int arr[] = { -8, 1, 2, 3, 4, 5, -2, -3 };
+    int key = 1;
+    int n, l, r;
+    n = sizeof(arr) / sizeof(arr[0]);
+    l = 0;
+    r = n - 1;
+    int index;
+ 
+    
+    index = findBitonicPoint(arr, n, l, r);
+ 
+    int x = searchBitonic(arr, n, key, index);
+ 
+    if (x == -1)
+        cout << "Element Not Found" << endl;
+    else
+        cout << "Element Found at index " << x << endl;
+ 
+    return 0;
+}
